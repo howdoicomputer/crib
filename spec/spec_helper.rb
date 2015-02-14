8b2ffaef7ae8e37bec5f42ee62ca019dbf05560b
Rafal Chmiel <rafalmarekchmiel@gmail.com>
2015-02-14 17:02:25 +0000

    Remove unnecessary RSpec filtering options

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,8 +14,6 @@ WebMock.disable_net_connect!(allow_localhost: true)
 require 'support/github'
 
 RSpec.configure do |config|
-  config.run_all_when_everything_filtered = true
-  config.filter_run :focus
   config.order = 'random'
   config.before(:each) { stub_request(:any, /api.github.com/).to_rack(GitHub) }
 end
